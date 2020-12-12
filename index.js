6b237c0cf20495816732eac0259cc3fb93e1f040
tbanish <tombanish@gmail.com>
2020-12-12 10:38:47 -0500

    commit tree 613bd2f2ac475ea0dd41eaa7a18f9aa771004bf5

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+  const fn = function () {
+    return drivers.slice(0,2)
+  }
+  return fn()
+}
+
+const returnLastTwoDrivers = function (drivers) {
+  const fn = function () {
+    return drivers.slice(2)
+  }
+  return fn()
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(integer) {
+  const fareMultiplier = function (fare) {
+    return fare * integer
+  }
+  return fareMultiplier
+}
+
+const fareDoubler = function (fare) {
+  return createFareMultiplier(2)(fare)
+}
+
+const fareTripler = function (fare) {
+  return createFareMultiplier(3)(fare)
+}
+
+function selectDifferentDrivers(drivers, selectingDrivers) {
+  return selectingDrivers(drivers)
+}
